95c530c55e9c7af2d83bc8d4bd16bbb113b9512f
wkrzywiec <wojciechkrzywiec@gmail.com>
2021-10-19 10:02:35 +0200

    working example

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import io from 'socket.io-client'; 
 import SockJsClient from 'react-stomp';
+import { Client } from '@stomp/stompjs';
+import * as SockJS from 'sockjs-client';
 
 function App() {
 
@@ -10,6 +12,8 @@ function App() {
 
   const handleMsgSTOMP = (msg, topic)=> {
     console.log('STOMP: \t\t' + msg.time + "\t" + msg.text)
+    let str = '<p>React-Stomp: ' + msg.time + '&nbsp;&nbsp;&nbsp;&nbsp; ' + msg.text + '</p>' 
+    document.getElementById('messege-output').innerHTML += str
   }
 
   const handleDisconnectSTOMP = () =>  {
@@ -22,23 +26,58 @@ function App() {
     clientRef.connect();
   }
 
+  // useEffect(() => {
+  //   var clientS = new Client();
+  //   clientS.webSocketFactory =  () => {
+  //     return new SockJS("http://localhost:8080/chat");
+  //   };
+      
+  //   // 'http://localhost:8080/chat';
+
+  //   clientS.onConnect = function (frame) {
+  //     console.log('The StompJs has connected with the server');
+  //   };
+
+  //   let callback = function (message) {
+  //     // called when the client receives a STOMP message from the server
+      
+  //   };
+
+  //   console.log(clientS)
+
+  //   // clientS.activate();
+
+  //   clientS.subscribe('/topic/chat', (message) => {
+  //     if (message.body) {
+  //       console(message.body)
+  //     } else {
+  //       console('empty?: ' + message.body)
+  //     }
+  //   });
+
+  //   clientS.activate();
+  // }, []);
+
   // Socket.io library
 
   let socket = null
 
   useEffect(() => {
         // connect to the socket server
+        // socket = io('https://project-management-microservice-691.core-staging.umetrics.ninja:8081/chat', {
         socket = io('http://localhost:8081/chat', {
           transports: ['polling', 'websocket']
         });
     
         socket.on('connect', function () {
-          console.log('The client has connected with the server');
+          console.log('The Socket.io has connected with the server');
         });
     
         socket.on('chat', function (data) {
           // console.log(socket)
           console.log('Socket.io: \t' + data.time, "\t", data.text);
+          let str = '<p>Socket.io: ' + data.time + '&nbsp;&nbsp;&nbsp;&nbsp; ' + data.text + '</p>' 
+          document.getElementById('messege-output').innerHTML += str
             
         });
         
@@ -69,8 +108,10 @@ function App() {
       <div>
         <button onClick={handleReconnectSocketIo}>ReConnect Socket.io</button>
       </div>
+      <div id="messege-output"></div>
       <div>
-        <SockJsClient url={ "http://localhost:8080/chat" } topics={["/topic/chat"]} 
+        {/* <SockJsClient url={ "https://project-management-microservice-691.core-staging.umetrics.ninja/chat" } topics={["/topic/chat"]}  */}
+         <SockJsClient url={ "http://localhost:8080/chat" } topics={["/topic/chat"]} 
             onMessage={handleMsgSTOMP} ref={ (client) => { clientRef = client }}
             debug={ false }/>
       </div>
